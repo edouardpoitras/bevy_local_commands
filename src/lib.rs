b21a98c72310cbf5361b4f9fed6cb78112e81c6e
Tim Jentzsch <commits@timjen.net>
2024-01-10 18:59:51 +0100

    Add builder pattern functions to LocalCommand

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::tasks::IoTaskPool;
 use bevy::tasks::Task;
+use std::ffi::OsStr;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -48,8 +49,61 @@ pub struct LocalCommand {
 }
 
 impl LocalCommand {
-    pub fn new(command: Command) -> Self {
-        Self { command }
+    pub fn new<S>(program: S) -> Self
+    where
+        S: AsRef<OsStr>,
+    {
+        Self {
+            command: Command::new(program),
+        }
+    }
+
+    pub fn arg<S: AsRef<OsStr>>(mut self, arg: S) -> Self {
+        self.command.arg(arg);
+        self
+    }
+
+    pub fn args<I, S>(mut self, args: I) -> Self
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        self.command.args(args);
+        self
+    }
+
+    pub fn env<K, V>(mut self, key: K, val: V) -> Self
+    where
+        K: AsRef<OsStr>,
+        V: AsRef<OsStr>,
+    {
+        self.command.env(key, val);
+        self
+    }
+
+    pub fn envs<I, K, V>(mut self, vars: I) -> Self
+    where
+        I: IntoIterator<Item = (K, V)>,
+        K: AsRef<OsStr>,
+        V: AsRef<OsStr>,
+    {
+        self.command.envs(vars);
+        self
+    }
+
+    pub fn env_remove<K: AsRef<OsStr>>(mut self, key: K) -> Self {
+        self.command.env_remove(key);
+        self
+    }
+
+    pub fn env_clear(mut self) -> Self {
+        self.command.env_clear();
+        self
+    }
+
+    pub fn current_dir<P: AsRef<std::path::Path>>(mut self, dir: P) -> Self {
+        self.command.current_dir(dir);
+        self
     }
 }
 
