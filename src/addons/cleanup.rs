5c6912bd85df171b477e1f5f2e6e5fb551766a1f
Edouard Poitras <eddie@quantumcyberdefence.com>
2024-08-23 16:09:15 -0400

    TEMP

--- src/addons/cleanup.rs
+++ src/addons/cleanup.rs
@@ -1,34 +1,25 @@
+use std::marker::PhantomData;
+
 use bevy::prelude::*;
 
-use crate::{process::Process, Chain, Delay, LocalCommand, LocalCommandState, Retry};
+use crate::{process::Process, LocalCommand, LocalCommandState};
 
 #[derive(Debug, Component)]
-pub enum Cleanup {
-    DespawnEntity,
-    RemoveComponents,
+pub struct Cleanup<T: Component> {
+    data: PhantomData<T>
 }
 
 /// Clean up any completed processes according to the Cleanup component.
 ///
 /// Processes without the Cleanup component are ignored.
-pub(crate) fn cleanup_completed_process(
+pub(crate) fn cleanup_completed_process<T: Component>(
     mut commands: Commands,
-    query: Query<(Entity, &LocalCommand, &Cleanup)>,
+    query: Query<(Entity, &LocalCommand), With<Cleanup<T>>>,
 ) {
-    for (entity, local_command, cleanup) in query.iter() {
+    for (entity, local_command) in query.iter() {
         if let LocalCommandState::Done(_) = local_command.state {
-            match cleanup {
-                Cleanup::DespawnEntity => {
-                    if let Some(mut entity_commands) = commands.get_entity(entity) {
-                        entity_commands.despawn();
-                    }
-                },
-                Cleanup::RemoveComponents => {
-                    if let Some(mut entity_commands) = commands.get_entity(entity) {
-                        entity_commands
-                            .remove::<(Process, Chain, Delay, Retry, Cleanup, LocalCommand)>();
-                    }
-                },
+            if let Some(mut entity_commands) = commands.get_entity(entity) {
+                entity_commands.remove::<(LocalCommand, Process, T, Cleanup<T>)>();
             }
         }
     }
